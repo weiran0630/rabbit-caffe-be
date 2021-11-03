fd6bb100b4543657360a6282f9146344d09bd3c3
Wei-ran <wei-ran@tuta.io>
2021-11-03 20:44:31 +0800

    init

--- config/env/production/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 8080),
-  admin: {
-    auth: {
-      secret: env("ADMIN_JWT_SECRET", "54b8cdeda85afd74618c08d16fc61f31"),
-    },
-  },
-});
